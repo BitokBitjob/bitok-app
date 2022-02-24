98b18321d921c4a222080db0896472b24ca9f33f
mhhpc <93550340+mhhpc@users.noreply.github.com>
2022-02-24 19:57:04 +0330

    go to recyclerFragment part2

--- app/src/main/java/com/example/bitjobapk/RecyclerFragment.kt
+++ app/src/main/java/com/example/bitjobapk/RecyclerFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bitjobapk.databinding.FragmentLoginBinding
@@ -14,10 +15,35 @@ class RecyclerFragment : AppCompatActivity() {
 
     private lateinit var newRecyclerView: RecyclerView
     private lateinit var newArrayList: ArrayList<Events>
+    lateinit var imageId : Array<Int>
+    lateinit var timeId : Array<String>
+    lateinit var titleId : Array<String>
+    lateinit var nameId : Array<String>
+    lateinit var descriptionId : Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_recycler)
+
+        imageId = arrayOf(
+            R.drawable.time
+        )
+
+        timeId = arrayOf(
+            "12:00"
+        )
+
+        titleId = arrayOf(
+            "Event"
+        )
+
+        nameId = arrayOf(
+            "AliReza"
+        )
+
+        descriptionId = arrayOf(
+            "Powered by bitok"
+        )
 
         newRecyclerView = findViewById(R.id.recyclerview)
         newRecyclerView.layoutManager = LinearLayoutManager(this)
@@ -31,7 +57,12 @@ class RecyclerFragment : AppCompatActivity() {
 
     private fun getUserdata() {
 
+        for(i in imageId.indices){
 
+            val events = Events(imageId[i],timeId[i],titleId[i],nameId[i],descriptionId[i])
+            newArrayList.add(events)
+
+        }
 
         newRecyclerView.adapter = MyAdapter(newArrayList)
     }
