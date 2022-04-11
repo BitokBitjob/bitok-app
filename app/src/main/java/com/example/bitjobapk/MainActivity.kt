633a8d08f947772896d90817ca0b89495219dc7b
mhhpc <93550340+mhhpc@users.noreply.github.com>
2022-04-11 13:15:07 +0430

    fix bugs

--- app/src/main/java/com/example/bitjobapk/MainActivity.kt
+++ app/src/main/java/com/example/bitjobapk/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.bitjobapk.ui.main.EventListFragment
 import com.example.bitjobapk.ui.main.ProfileFragment
@@ -20,37 +21,18 @@ class MainActivity : AppCompatActivity() {
 
     private  lateinit var navController: NavController
 
-    private lateinit var navView : NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val eventlistFragment = EventListFragment()
-        val registeredEventFragment = RegisteredEventFragment()
-        val profileFragment = ProfileFragment()
-        makeCurrentFragment(eventlistFragment)
-
-        navView.setNavigationItemSelectedListener {
-            when (it.itemId){
-                R.id.ic_event -> makeCurrentFragment(eventlistFragment)
-                R.id.ic_registered_event -> makeCurrentFragment(registeredEventFragment)
-                R.id.ic_profile -> makeCurrentFragment(profileFragment)
-            }
-            true
-        }
-
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragment) as NavHostFragment
         navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        setupWithNavController(bottomNavigationView , navController)
     }
-
-    private fun makeCurrentFragment(fragmentContainerView: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment, fragmentContainerView)
-            commit()
-        }
 
     override fun onSupportNavigateUp(): Boolean {
         return  navController.navigateUp() ||  super.onSupportNavigateUp()
